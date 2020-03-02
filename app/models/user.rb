5d5388cd6ced3ad65dd9540b7e9ae301d31e1bb1
Sarah Harrington <sarah.harrington@iorahealth.com>
2020-03-02 16:36:46 -0600

    users can sign up with password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     length: { maximum: 255 },
                     uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  before_save :downcase_email
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
