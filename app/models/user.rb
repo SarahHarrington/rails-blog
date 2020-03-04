70464d56ff59d778d5e50ddb6e51ec7151bfb6db
Sarah Harrington <sarah.harrington@iorahealth.com>
2020-03-04 13:30:18 -0600

    persistant log in now available

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+attr_accessor :remember_token
+
   validates :name, presence: true
   validates :email, presence: true,
                     length: { maximum: 255 },
@@ -11,5 +13,32 @@ class User < ApplicationRecord
 
   def downcase_email
     self.email = email.downcase
+  end
+
+  # returns a hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost:cost)
+  end
+
+  # returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Rememers the user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
