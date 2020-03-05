d66bcf0a8e7fc0db63cd08cd4d610a166efcb7a0
Sarah Harrington <sarah.harrington@iorahealth.com>
2020-03-05 16:36:34 -0600

    add seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Create a sample user
+User.create(name: 'Example User',
+            email: 'sarah@sarahdoes.tech',
+            password: 'foobar',
+            password_confirmation: 'foobar')
+
+99.times do
+  first_name = Faker::Creature::Cat.name
+  last_name = Faker::Creature::Cat.breed
+  email = Faker::Internet::email(name: first_name)
+  password = "password"
+  User.create!( name: first_name + ' ' + last_name,
+                email: email,
+                password: password,
+                password_confirmation: password)
+end
