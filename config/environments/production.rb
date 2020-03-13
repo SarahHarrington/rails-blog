4c50316408bc1e34b70174505a7288281c175fb5
Sarah Harrington <sarah.harrington@iorahealth.com>
2020-03-13 11:00:58 -0500

    add heroku sendgrid integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,9 +62,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'lit-everglades-04601.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com'
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+    :api_key => ENV['SENDGRID_API_KEY']
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
