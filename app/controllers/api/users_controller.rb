ff4970c542a7398ca83b86143d7cf5954156692f
Sarah Harrington <sarah.harrington@iorahealth.com>
2020-05-14 16:16:53 -0500

    Add api user controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,77 @@
+module Api
+  class UsersController < ApplicationController
+  # before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  # before_action :correct_user, only: [:edit, :update]
+  # before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # @user.send_activation_email
+      render json: @user
+    else
+      render json: @user
+    end
+  end
+
+  def update
+    # @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  #confirms the user is logged in
+  def logged_in_user
+    store_location
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    #since @user is defined here, it was removed from the edit and update methods
+    @user = User.find(params[:id])
+    # redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+end
+end
