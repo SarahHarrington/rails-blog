043b4ae2fcc0ef392209ddea9acedbea90e9a3bb
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-05-25 16:19:26 -0500

    fix typo

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,7 +2,7 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
 
-    if user && !user.activated?)
+    if user && !user.activated?
       user.activate
       #add some sort of flash
       log_in user
