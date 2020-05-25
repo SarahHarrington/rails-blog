e2becf5692d5523dcf56a2771617d2a852e24c24
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-05-25 18:32:56 -0500

    update cors config

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.middleware.insert_before 0, Rack::Cors do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins 'https://mysterious-refuge-67427.herokuapp.com/'
         resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
