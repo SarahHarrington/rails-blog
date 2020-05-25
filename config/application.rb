6ccd108de4e23cec598491643acde9b1dd7bfba9
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-05-25 15:52:10 -0500

    update cors

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,8 @@ module Blog
     config.load_defaults 6.0
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://mysterious-refuge-67427.herokuapp.com/'
-        resource '*', 
-        headers: :any, 
-        methods: [:get, :post, :delete, :put, :patch, :options, :head]
+        origins 'https://mysterious-refuge-67427.herokuapp.com/signup'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
       end
     end
     config.api_only = true
