015cb1ce549492fb88fe85de703884dfd2379133
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-05-25 14:45:28 -0500

    update origin

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Blog
     config.load_defaults 6.0
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'https://mysterious-refuge-67427.herokuapp.com/'
         resource '*', 
         headers: :any, 
         methods: [:get, :post, :delete, :put, :patch, :options, :head]
