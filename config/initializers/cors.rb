b45baef7d08ddea8c60b09b3e6ec44778c77c425
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-05-25 22:28:08 -0500

    trying a different origin?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'herokuapp.com'
+    origins 'mysterious-refuge-67427.herokuapp.com'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
