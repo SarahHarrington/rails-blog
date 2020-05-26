17b20274fcf5942b53b37f083c5162340cefdac4
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-05-25 21:00:05 -0500

    one more time

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'https://mysterious-refuge-67427.herokuapp.com/'
       resource '*', 
-        headers: 'x-domain-token', 
+        headers: :any, 
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
