ad22beeaf29481e5d315b440dfaa50a0998a9623
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-05-25 22:07:07 -0500

    fixing ()

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource ('*', 
-        headers: :any, 
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      )
-    end
+  allow do
+    origins '*'
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+end
 
 
-  Rails.application.config.hosts << "mysterious-refuge-67427.herokuapp.com"
-  Rails.application.config.hosts << "mysterious-refuge-67427.herokuapp.com"
+Rails.application.config.hosts << "mysterious-refuge-67427.herokuapp.com"
+Rails.application.config.hosts << "mysterious-refuge-67427.herokuapp.com"
   
