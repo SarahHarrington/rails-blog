bd0a81ddbe784c199f853e857ab83e42f725a2cf
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-05-25 20:00:49 -0500

    trying something else

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://mysterious-refuge-67427.herokuapp.com/', 'https://lit-everglades-04601.herokuapp.com/'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      origins 'https://mysterious-refuge-67427.herokuapp.com/'
+      resource '*', headers: 'x-domain-token', methods: [:get, :post, :patch, :put]
     end
   end
 
 
   Rails.application.config.hosts << "mysterious-refuge-67427.herokuapp.com"
-  Rails.application.config.hosts << "lit-everglades-04601.herokuapp.com"
+  
